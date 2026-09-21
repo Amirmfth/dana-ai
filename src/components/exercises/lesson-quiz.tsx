55d906c70c679b1282ef1041f82a63b5374dedd5
Amir.mohamad <102855219+Amirmfth@users.noreply.github.com>
2026-09-21 15:51:37 +0330

    fix: remove unused quiz import

--- src/components/exercises/lesson-quiz.tsx
+++ src/components/exercises/lesson-quiz.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useState } from "react";
 
 import { ExerciseQuestion } from "@/components/exercises/exercise-question";
 import { AnimatedProgress } from "@/components/ui/animated-progress";
